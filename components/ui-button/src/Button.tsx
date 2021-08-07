3314895dbd29abb64c745b699a8cd76fd5a87172
btmnk <contact@bitmonk.de>
2021-08-07 23:35:44 +0200

    Adds experimental rollup setup

---
+++ components/ui-button/src/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Button.css';
+
+export const Button: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
